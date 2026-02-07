ff7ab773fbc3bdb0cf6485a9217e205322829f36
Léo Mathurin <leo.mathurin@epitech.eu>
2026-02-07 19:01:15 +0100

    init environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    sanity({
+      // Astro does not directly support environment variables in the config file
+      // https://docs.astro.build/en/guides/environment-variables/#in-the-astro-config-file
+      projectId: "sjagkr9a",
+      dataset: "production",
+      studioBasePath: '/studio'
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
